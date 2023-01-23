656fc5a6347865aaa34dfe1b421a165a50b74c84
shashank <shashank944533@gmail.com>
2023-01-23 16:00:33 +0530

    first commit sorting of elements

---
+++ sortingtwoclass/src/sortingtwoclass/SortingApp.java
@@ -0,0 +1,36 @@
+package sortingtwoclass;
+
+import java.util.Scanner;
+
+public class SortingApp {
+public static void main(String []args)
+{
+	Scanner scan = new Scanner(System.in);
+	System.out.println("Enter the array length");
+	int n = scan.nextInt();
+	int arr[] = new int[n];
+	System.out.println("Enter array contents:");
+	for(int i = 0; i<=arr.length-1;i++)
+	{
+		System.out.println("Enter a number");
+		arr[i] = scan.nextInt();
+		
+	}
+	
+	System.out.println("Array contents before sorting");
+	for(int i = 0;i<=arr.length-1;i++)
+	{
+		System.out.println(arr[i]+"");
+		
+	}
+	System.out.println();
+	
+	Sorting s = new Sorting();
+	s.insertionSort(arr);
+	System.out.println("Array contents after sorting:");
+	for(int i = 0;i<= arr.length-1;i++)
+	{
+		System.out.println(arr[i] + " ");
+	}
+}
+}
